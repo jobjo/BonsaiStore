732cc124ee5d338078d5e2689ffba255ce036c30
Joel Bjornson <joel.bjornson@gmail.com>
2014-12-20 22:22:44 +0100

    Adding missing files

---
+++ benchmarks/BonsaiStore.Benchmarks/SalesItemReports.fs
@@ -0,0 +1,69 @@
+﻿namespace FSharp.BonsaiStore.Benchmarks
+module SalesItemsReports =
+    open System
+    open Microsoft.FSharp.Quotations
+    open FSharp.BonsaiStore
+    open Microsoft.FSharp.Quotations
+    open Microsoft.FSharp.Linq.QuotationEvaluation
+    open SalesItems
+
+    module SB = FSharp.BonsaiStore.StoreBuilder
+    module U = FSharp.BonsaiStore.Benchmarks.Utils
+
+    let toPredicate (exp: Expr<SalesItem -> bool>) = exp.Compile() ()
+
+    /// Count using map reduce
+    let totSalesStoreMapReduce (store: IBonsaiStore<SalesItem>) (filter: Expr<SalesItem-> bool>) =
+        SB.report store filter 0.  (fun si -> si.Price) Array.sum
+
+    /// Count using map reduce
+    let totSalesStoreMapReduceParallel (store: IBonsaiStore<SalesItem>) (filter: Expr<SalesItem-> bool>) =
+        SB.reportParallel store filter 0.  (fun si -> si.Price) Array.sum
+
+    /// Count using map reduce
+    let totSalesStoreArray (items: SalesItem []) (filter: Expr<SalesItem-> bool>) =
+        let pred = toPredicate filter
+        (0., items)
+        ||> Array.fold (fun c si ->
+            si.Price + if pred si then si.Price else 0.
+        )
+
+    /// Count items
+    let totalSalesBenchMark () =
+        let numItems = int 1e5
+
+        printfn "Generate %A sales items" numItems
+        let items = generateSalesItems numItems
+        printfn "Memory %A" (Utils.getCurrentMemory())
+
+        printfn "Build store"
+        let store = SB.buildStore items
+        printfn "Memory %A" (Utils.getCurrentMemory())
+
+        let oneDayFilter = <@ fun (si: SalesItem) -> si.Date = new DateTime(2010,12,25) @>
+        let oneMonthFilter =
+            <@ fun (si: SalesItem) -> 
+                si.Date >= new DateTime(2010,1,1) && si.Date <= new DateTime(2011,2,1) 
+            @>
+        let oneYearFilter = 
+            <@ fun (si: SalesItem) -> 
+                si.Date >= new DateTime(2010,1,1) && si.Date <= new DateTime(2011,1,1) 
+            @>
+
+        let allTrue = 
+            <@ fun (si: SalesItem) ->  true @>
+
+        let test filter =
+            [
+                "Store", Utils.testCase <| fun _ -> totSalesStoreMapReduce store filter
+                "Store (P)", Utils.testCase <| fun _ -> totSalesStoreMapReduceParallel store filter
+                "Array", Utils.testCase <| fun _ -> totSalesStoreArray items filter
+            ]
+        [
+            "Total Sales One Day", test oneDayFilter
+            "Total Sales One Month", test oneMonthFilter
+            "Total Sales One Year", test oneYearFilter
+            "All", test allTrue
+
+        ]
+        |> Utils.benchmark 10
---
+++ benchmarks/BonsaiStore.Benchmarks/SalesItems.fs
@@ -0,0 +1,83 @@
+﻿namespace FSharp.BonsaiStore.Benchmarks
+module SalesItems =
+    open FSharp.BonsaiStore
+    open System
+    module SB = FSharp.BonsaiStore.StoreBuilder
+
+    type Employee = { EmployeeId : int ; Name : string; Team : string }
+    type Product = { ProductId : int; Category : string ; Name : string }
+    type SalesItem = 
+        {
+            Product : Product
+            Quantity : int
+            Price : float
+            Date : System.DateTime
+            Employee : Employee 
+        }
+        [<Index; Level(0)>]
+        member private this.DateIx() = 
+            let span = this.Date - DateTime(1900,1,1)
+            span.Days / 1
+//
+//        [<Index; Level(1)>]
+//        member private this.PriceIx() = int (this.Price / 100.)
+//
+//        [<Index; Level(2)>]
+//        member private this.PersonNameIx() = this.Employee.Name.GetHashCode()
+    
+    let generateSalesItems n =
+        let r = new Random()
+        let genName =
+            let fNames = [|"Abdul"; "Andras"; "Anna"; "Bo"; "Cynthia"; "Jennifer"; "Gabor"; "John"; "Michele"; "Atilla"; "Balazs"; "Johny"; "Nora"; |]
+            let lNames = [|"Abramsson"; "Johnson"; "Williams"; "Pearson"; "Smith"; "Davis"; "Georgia"; "White"; "Black"; "Miller"; "ONeal"; "Stockton"|]
+            fun () ->
+                let fName = fNames.[r.Next() % fNames.Length]
+                let lName = lNames.[r.Next() % lNames.Length]
+                fName + " " + lName
+        let genTeam =
+            let names = [| "Olympos"; "Team Berlin"; "A-Team"; "Z-Team"; "Team France"; "Rocket Builders"; |]
+            fun () ->
+                names.[r.Next() % names.Length]
+        let genCategory =
+            let cats = [| "Kitchen"; "Outdoor"; "All Around"; "Premium"; |]
+            fun () -> cats.[r.Next() % cats.Length]
+        let genProductName =
+            let types = [| "Knot"; "Bolt"; "Grip"; "Tape"; "Sink"; "Chain"; "Ring" ; "Washer" |]
+            let fs = [| "Space"; "Smurf"; "Alpha"; "Helium"; "Rubber" ; "Metal" ; "Party"|]
+            let ats = [| "Big"; "Thin" ; "Thick" ; "Small" ; "XXL" ; "M"; "S"; "L" |]
+            let codes = [| "SKO"; "FXL"; "VPK"; "KD"; "M"; "SD" |]
+            fun () ->
+                sprintf "%s %s %s %s" 
+                    ats.[r.Next() % ats.Length]
+                    fs.[r.Next() % fs.Length]
+                    types.[r.Next() % types.Length]
+                    codes.[r.Next() % codes.Length]
+        let employees =
+            [
+                for i in [1 .. 1000] do
+                    let name = genName()
+                    let team = genTeam ()
+                    yield {EmployeeId = i; Name = name; Team = team}
+            ]
+        let products = 
+            [
+                for i in [1 .. 1000] do
+                    yield { ProductId = i; Category = genCategory(); Name = genProductName()}
+            ]
+        let dates =
+            let d0 = DateTime(2000, 1, 1)
+            [for d in [1 .. 365 * 4] do yield d0.AddDays(float d)]
+        [|
+            for i in [1 .. n] do
+                let prod =  products.[r.Next() % products.Length]
+                let qnt = 1 + r.Next() % 10
+                let price = float <| prod.Name.Length * 10 * qnt
+                yield {
+                    Product = prod
+                    Quantity = qnt
+                    Price = price
+                    Date = dates.[r.Next() % dates.Length]
+                    Employee = employees.[r.Next() % employees.Length]
+                }
+        |]
+
