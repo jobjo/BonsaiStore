df80a04ef16dcb00d4c8301e78b12ee1c717c1ae
Joel Bjornson <joel.bjornson@gmail.com>
2014-12-30 10:36:38 +0100

    Add missing file

---
+++ benchmarks/BonsaiStore.Benchmarks/ArrayStore.fs
@@ -0,0 +1,17 @@
+﻿namespace FSharp.BonsaiStore.Benchmarks
+module ArrayStore =
+    open FSharp.BonsaiStore
+    let buildStore items =
+        let report choose filter (mr: MapReducer<_,_>) =
+            let pred = Quatations.compileQuatationFilter filter
+            items
+            |> choose (fun x ->
+                if pred x then 
+                    Some (mr.Map x)
+                else 
+                    None
+            )
+            |> mr.Reduce
+        { new IStore<'T> with
+            member this.Report exp mr = report Array.choose exp mr
+        }
